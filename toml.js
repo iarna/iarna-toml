14b9154d34ac490e858da21a11afe53d4cd5c5e1
Rebecca Turner <me@re-becca.org>
2018-06-10 00:11:39 -0700

    Fallback to using toml, if toml-j fails
    toml-j fails on larger TOML files

--- toml.js
+++ toml.js
@@ -1,4 +1,16 @@
 'use strict'
-const parse = require('toml-j0.4').parse
-exports.parse = data => parse(data.toString())
+const parsej = require('toml-j0.4').parse
+let parset
+exports.parse = data => {
+  try {
+    return parsej(data.toString())
+  } catch (ex) {
+    if (!parset) require('toml').parse
+    try {
+      return parset(data)
+    } catch (_) {
+      throw ex
+    }
+  }
+}
 exports.stringify = require('./toml-stringify.js')
