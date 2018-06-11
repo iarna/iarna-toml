06726ed8f95f2c60aa1154e77b6932df5f0f4fb3
Rebecca Turner <me@re-becca.org>
2018-06-10 20:19:20 -0700

    Revert "Fallback to using toml, if toml-j fails"
    This reverts commit 14b9154d34ac490e858da21a11afe53d4cd5c5e1.

--- toml.js
+++ toml.js
@@ -1,16 +1,4 @@
 'use strict'
-const parsej = require('toml-j0.4').parse
-let parset
-exports.parse = data => {
-  try {
-    return parsej(data.toString())
-  } catch (ex) {
-    if (!parset) require('toml').parse
-    try {
-      return parset(data)
-    } catch (_) {
-      throw ex
-    }
-  }
-}
+const parse = require('toml-j0.4').parse
+exports.parse = data => parse(data.toString())
 exports.stringify = require('./toml-stringify.js')
