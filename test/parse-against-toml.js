7e9f2e72cfdc675dc0b6307593daa96392f663d6
Rebecca Turner <me@re-becca.org>
2018-06-16 01:50:27 -0700

    Also test spec examples against spec YAML

--- test/parse-against-toml.js
+++ test/parse-against-toml.js
@@ -5,14 +5,25 @@ const test = require('tap').test
 const TOML = require('../toml.js')
 const toml = require('toml')
 const tomlj = require('toml-j0.4')
+const YAML = require('js-yaml')
 const files = ['example-v0.3.0.toml', 'example-v0.4.0.toml', 'example.toml', 'hard_example.toml', 'hard_example_unicode.toml']
+const yamlPairs = {
+  'example.toml': 'example.yaml',
+  'hard_example.toml': 'hard_example.yaml'
+}
+
+function readFile (file) {
+  return fs.readFileSync(path.join(__dirname, 'fromspec', file), 'utf8')
+}
 
 test('spec-examples', function (t) {
-  t.plan(files.length * 2)
+  t.plan((files.length * 2) + Object.keys(yamlPairs).length)
   files.forEach(function (file) {
-    const ourValue = TOML.parse(fs.readFileSync(path.join(__dirname, 'fromspec', file), 'utf8'))
-    const tomlValue = toml.parse(fs.readFileSync(path.join(__dirname, 'fromspec', file), 'utf8'))
-    const tomljValue = tomlj.parse(fs.readFileSync(path.join(__dirname, 'fromspec', file), 'utf8'))
+    const ourValue = TOML.parse(readFile(file))
+    const tomlValue = toml.parse(readFile(file))
+    const tomljValue = tomlj.parse(readFile(file))
+    const yamlValue = yamlPairs[file] && YAML.safeLoad(readFile(yamlPairs[file]))
+    if (yamlValue) t.isDeeply(ourValue, yamlValue, file + " spec's YAML representation")
     t.isDeeply(ourValue, tomlValue, file + ' using toml')
     t.isDeeply(ourValue, tomljValue, file + ' using toml-j0.4')
   })
