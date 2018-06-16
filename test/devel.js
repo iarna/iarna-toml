2c91a7228a17a874b34c8eb2bf52b5e578beda64
Rebecca Turner <me@re-becca.org>
2018-06-15 20:43:28 -0700

    Add tests for devel mode

---
+++ test/devel.js
@@ -0,0 +1,18 @@
+'use strict'
+const test = require('tap').test
+const TOMLParser = require('../lib/toml-parser').makeParserClass(require('../lib/parser-debug.js'))
+
+let testtoml = `a = [[1.0], [1], [{}], ['abc']]\n[[b]]\n[c]\n`
+
+test('devel', t => {
+  t.plan(1)
+  console.log('Parsing:', testtoml)
+  const parser = new TOMLParser()
+  try {
+    parser.parse(testtoml)
+    t.isDeeply(parser.finish(), {a: [[1], [1], [{}], ['abc']], b: [{}], c: {}}, 'parsed with debugging')
+  } catch (ex) {
+    t.comment(ex)
+    t.fail('parsed with debugging')
+  }
+})
