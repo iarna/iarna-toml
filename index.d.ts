789d33ec3b062819f03aa014fac728f02e7ce88e
牛牛/MomoCow <momocow.me@gmail.com>
2018-08-28 22:15:12 +0800

    Fix no-import-statement in type definitions.

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
-import { Readable } from 'stream'
-
 declare module "@iarna/toml" {
   interface FuncParse {
     (toml: string): object
     async: (toml: string, options: { blocksize: number }) => Promise<object>
-    stream: (readable: Readable) => Promise<object>
+    stream: (readable: NodeJS.ReadableStream) => Promise<object>
   }
 
   export const parse: FuncParse
