8311db8c4ff3c8dd6ffc8cb487606117be4af2c5
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2018-09-04 20:25:46 -0400

    Improve type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,16 @@
 declare module "@iarna/toml" {
+  type AnyJson = boolean | number | string | null | JsonArray | JsonMap
+  interface JsonArray extends Array<AnyJson> {}
+  interface JsonMap {
+    [key: string]: AnyJson;
+  }
+  
   interface FuncParse {
-    (toml: string): object
+    (toml: string): JsonMap
     async: (toml: string, options: { blocksize: number }) => Promise<object>
     stream: (readable: NodeJS.ReadableStream) => Promise<object>
   }
 
   export const parse: FuncParse
-  export function stringify (obj: object): string
+  export function stringify (obj: JsonMap): string
 }
