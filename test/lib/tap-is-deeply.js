f8bc51d99020d0432c8df49f919e787473c326a0
Rebecca Turner <me@re-becca.org>
2019-02-20 12:04:40 -0800

    spec: Rewrite how we determine equality
    This does two things:
    
    First it allows different libraries to use different numeric representations
    when they produce the same fidelity results.
    
    Second it allows for more varied date/time representations, which is
    important as JavaScript has no native way to represent TOML localtimes (and
    plain time values).

--- test/lib/tap-is-deeply.js
+++ test/lib/tap-is-deeply.js
@@ -10,12 +10,36 @@ if (!tap.deeplyObjectIs) {
   })
 }
 
+function kindOf (aa) {
+  const aaType = typeof aa
+  if (aaType === 'number' || aaType === 'bigint') return 'numberish'
+  if (aa instanceof Date) return 'date'
+  if (Array.isArray(aa)) return 'array'
+  if (Buffer.isBuffer(aa)) return 'buffer'
+  return aaType
+}
+
 function isObjectDeeply (aa, bb) {
-  if (typeof aa !== typeof bb) return false
-  if (aa == null || bb == null || typeof aa !== 'object') return Object.is(aa, bb)
-  if (Array.isArray(aa) && Array.isArray(bb)) return isArrayDeeply(aa, bb)
-  if (Array.isArray(aa) || Array.isArray(bb)) return false
-  if (aa instanceof Date && bb instanceof Date) return aa.toISOString() === bb.toISOString()
+  if (kindOf(aa) !== kindOf(bb)) return false
+  if (Object.is(aa, bb)) return true
+  const kind = kindOf(aa)
+  if (kind === 'numberish') {
+    /* NOTE: This allows numbers to be compared to bigints.  They only count
+       as == if they represent the exact same value, so if a bigint was
+       needed to represent this number the check will fail, but if it can be
+       safely either, then either is allowed.  */
+    /* eslint-disable eqeqeq */
+    return aa == bb
+  }
+  if (kind === 'date') {
+    if (aa.toISOString().replace(/[.]0+$/, '') === bb.toISOString().replace(/[.]0+$/, '')) return true
+    if (Number(aa) === Number(bb)) return true
+    if (Number(new Date(aa.toISOString())) === Number(new Date(bb.toISOString()))) return true
+    return false
+  }
+  if (kind === 'array') return isArrayDeeply(aa, bb)
+
+  if (kind !== 'object') return false
   let aaKeys = Object.keys(aa)
   let bbKeys = Object.keys(bb)
   if (aaKeys.length !== bbKeys.length) return false
